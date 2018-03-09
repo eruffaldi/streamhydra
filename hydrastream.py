a852c7f3ef6241caf5fe2c7ef32739dca1f6783e
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2018-03-09 07:05:52 +0100

    py3 port

--- hydrastream.py
+++ hydrastream.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #TEST 
 #   curl http://127.0.0.1:8766 | python mp4streamtest.py  -
 #LIVE
@@ -11,12 +11,12 @@
 #Example: 
 
 import subprocess # for piping
-from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from http.server import HTTPServer, BaseHTTPRequestHandler
 import struct,os
 import select
-from Queue import Queue
+from queue import Queue
 from threading import Thread
-from SocketServer import ThreadingMixIn
+from socketserver import ThreadingMixIn
 import socket
 
 ATOM_HEADER = {
@@ -43,7 +43,7 @@ def waitsocket(s,timeout):
     socket_list = [s]
     read_sockets, write_sockets, error_sockets = select.select(socket_list , [], socket_list,timeout)
     if len(error_sockets) != 0:
-        print "waitsocket fail"
+        print("waitsocket fail")
         return False
     else:
         return True
@@ -55,7 +55,7 @@ class Source:
         self.moofwait = set()
         self.live = set()
         self.ready = False
-        self.prebuf = ""
+        self.prebuf = b""
         self.done = False
         self.hostport = hostport
         self.hostsocket = hostsocket
@@ -86,13 +86,13 @@ class Source:
             suffix= "  -nostdin -g 1 -preset ultrafast -vcodec libx264 -tune zerolatency -b:v 5000k -frag_duration 100 -f ismv - -f mjpeg tcp://127.0.0.1:%d" % self.hostport
             command = pickffmpegsource(self.what)
             command += suffix
-            print("mp4: running stdin:\n %s" % (command, ))
+            print(("mp4: running stdin:\n %s" % (command, )))
             subproc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=FNULL,bufsize=-1, shell=True)
             subprocfx = (lambda: subproc.poll() is None,lambda n: subproc.stdout.read(n),subproc.kill)
         else:
             (clientsocket, address) = self.hostsocket.accept()
             clientsocket.setblocking(0)
-            print "mp4 listening as",address
+            print("mp4 listening as",address)
             self.hostsocket.close()
             subprocfx = (lambda: waitsocket(clientsocket,200),lambda n: clientsocket.read(n),lambda: clientsocket.close())
 
@@ -101,23 +101,23 @@ class Source:
         while subprocfx[0]():
             try:
                 msg = self.queue.get(False)
-            except:
+            except: 
                 msg = None
             if msg is not None:
-                print "MAIN msg",msg
+                print("MAIN msg",msg)
                 if msg[0] == "add":
                     l = msg[1]
                     if self.ready:
-                        print "MAIN adding listener in MOOF mode using prefix",len(self.prebuf),l.id
+                        print("MAIN adding listener in MOOF mode using prefix",len(self.prebuf),l.id)
                         l.enqueue(self.prebuf)
                         self.moofwait.add(l)
                     else:
-                        print "MAIN adding listener in HEAD mode",l.id
+                        print("MAIN adding listener in HEAD mode",l.id)
                         self.headwait.add(l)
                         # wait for foll HEAD
                 elif msg[0] == "del":
                     l = msg[1]
-                    print "MAIN removing listener",l.id
+                    print("MAIN removing listener",l.id)
                     if l in self.headwait:
                         self.headwait.remove(l)
                     elif l in self.moofwait:
@@ -127,7 +127,7 @@ class Source:
 
             try:
                 stdoutdata = subprocfx[1](DataChunkSize)
-            except:
+            except Exception as e:
                 break
             # append to p arser
             ss.data += stdoutdata
@@ -136,10 +136,10 @@ class Source:
             for a,t,s in ss.ondata():
                 #print "\tMAIN part",t,a,len(s) if type(s) is str else s
                 if not self.ready:
-                    if a == StreamingAtom.BEGIN and t == "moof":
+                    if a == StreamingAtom.BEGIN and t == b"moof":
                         # unlock HEAD and MOOF pending
                         self.ready = True
-                        print "\t\tMAIN found head",len(prebuf),"with pending",len(self.headwait),len(self.moofwait)
+                        print("\t\tMAIN found head",len(prebuf),"with pending",len(self.headwait),len(self.moofwait))
                         if len(self.headwait) > 0:
                             for l in self.headwait:
                                 l.enqueue(self.prebuf)
@@ -152,7 +152,7 @@ class Source:
                         # accumulate
                         prebuf += s                
                 else:
-                    if len(self.moofwait) > 0 and a == StreamingAtom.BEGIN and t == "moof":
+                    if len(self.moofwait) > 0 and a == StreamingAtom.BEGIN and t == b"moof":
                         self.live |= self.moofwait
                         self.moofwait.clear()
                     elif a == StreamingAtom.BODY:
@@ -160,25 +160,25 @@ class Source:
                             l.enqueue(s)
                     
             self.prebuf = prebuf
-        print "MAIN subprocess mp4 exit"
+        print("MAIN subprocess mp4 exit")
         for x in self.headwait:
             try:
-                x.q.put("")
+                x.q.put(b"")
             except:
                 pass
         for x in self.moofwait:
             try:
-                x.q.put("")
+                x.q.put(b"")
             except:
                 pass
         for x in self.live:
             try:
-                x.q.put("")
+                x.q.put(b"")
             except:
                 pass
         try:
             subprocfx[2]()
-        except:
+        except Exception as e:
             pass   
         self.done = True
     def runjpeg(self):
@@ -192,14 +192,14 @@ class Source:
             suffix= " -nostdin -f mjpeg -q:v 3 -huffman optimal -"
             command = pickffmpegsource(self.what)
             command += suffix
-            print("mjpeg running stdin: %s" % (command, ))
+            print(("mjpeg running stdin: %s" % (command, )))
             subproc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=FNULL ,bufsize=-1, shell=True)            
             subprocfx = (lambda : subproc.poll() is None,lambda n: subproc.stdout.read(n),subproc.kill)
         else:
-            print "mjpeg accepting ",self.hostport
+            print("mjpeg accepting ",self.hostport)
             (clientsocket, address) = self.hostsocket.accept()
             clientsocket.setblocking(0)
-            print "mjpeg accepted ",address
+            print("mjpeg accepted ",address)
             self.hostsocket.close()
             subprocfx = (lambda: waitsocket(clientsocket,200),lambda n: clientsocket.recv(n),lambda: clientsocket.close())
 
@@ -211,7 +211,7 @@ class Source:
             except:
                 msg = None
             if msg is not None:
-                print "MAIN msg",msg
+                print("MAIN msg",msg)
                 if msg[0] == "add":
                     l = msg[1]
                     self.live.add(l)
@@ -221,8 +221,8 @@ class Source:
                         self.live.remove(l)
             try:
                 stdoutdata= subprocfx[1](DataChunkSize) 
-            except:
-                print "jpg error"
+            except Exception as e:
+                print("jpg error")
                 break
             if len(stdoutdata) != 0:
                 # append to p arser
@@ -233,34 +233,34 @@ class Source:
         #print "MAIN subprocess exit",subproc.returncode
         for x in self.live:
             try:
-                x.q.put("")
+                x.q.put(b"")
             except:
                 pass
         try:
             subprocfx[2]()
-        except:
+        except Exception as e:
             pass
 
 
 class StreamingJpeg:
     def __init__(self):
-        self.data = "" # buffer
-        self.buffer = "" # for whole
+        self.data = b"" # buffer
+        self.buffer = b"" # for whole
     def ondata(self):
         data = self.data
         while len(data) != 0:
-            k = data.find("\xFF\xD8")
+            k = data.find(b"\xFF\xD8")
             #print len(data),k,len(self.buffer)
             #print "jpeg",len(data),k
             if k < 0:
                 self.buffer += data
-                data = ""
+                data = b""
                 break
             else:
                 self.buffer += data[0:k]
                 if len(self.buffer) != 0:
                     yield self.buffer
-                self.buffer = "\xFF\xD8"
+                self.buffer = b"\xFF\xD8"
                 data = data[k+2:]
         self.data = data
 
@@ -272,8 +272,8 @@ class StreamingAtom:
         self.tag = None
         self.tagsize = 0
         self.left = 0
-        self.data = "" # buffer
-        self.buffer = "" # for whole
+        self.data = b"" # buffer
+        self.buffer = b"" # for whole
         self.whole= whole
     def ondata(self):
         data = self.data
@@ -286,7 +286,7 @@ class StreamingAtom:
                         # emit one tag
                         self.buffer += data[0:n]
                         yield (StreamingAtom.BODY,self.tag,self.buffer)
-                        self.buffer = ""
+                        self.buffer = b""
                     else:
                         # emit body and close
                         yield (StreamingAtom.BODY,self.tag,data[0:n])
@@ -352,56 +352,60 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
     def do_HEAD(self):
         self._writeheaders()
+    def do_OPTIONS(self):
+        print("HANDLER OPTIONS handling",self.path)
+        print("HANDLER headers",self.headers)
+        self._writeheaders()
 
     def write_chunk(self,chunk):
-        tosend = '%X\r\n'%(len(chunk))
+        tosend = b'%X\r\n'%(len(chunk))
         self.wfile.write(tosend)
         self.wfile.write(chunk)
-        self.wfile.write("\r\n")
+        self.wfile.write(b"\r\n")
 
     def enqueue(self,chunk):
         # TODO size limiter
         self.q.put(chunk)
     def do_GET(self):
-        print "HANDLER handling",self.path
+        print("HANDLER handling",self.path)
+        print("HANDLER headers",self.headers)
         if self.path == "/mp4":
-            print "HANDLER headers",self.headers
             self._writeheaders()
 
             self.q = Queue()
-            print "HANDLER adding",self.id
+            print("HANDLER adding",self.id)
             source.addlistener(self)
             while True:
                 try:
                     a = self.q.get(True,0.5) # make waitable
-                    if a == "": 
+                    if a == b"": 
                         break
-                except:
+                except Exception as e:
                     # empty
                     if source.done:
                         break
-                    a = ""
-                    print "HANDLER ping",self.id
+                    a = b""
+                    print("HANDLER ping",self.id)
                 #print "HANDLER write",len(a)
                 try:
                     self.write_chunk(a)
-                except:
-                    print "HANDLER network exception",self.id
+                except Exception as e:
+                    print("HANDLER /mp4 network exception",self.id,e)
                     break
             source.dellistener(self)
-            print "HANDLER removed",self.id
+            print("HANDLER removed",self.id)
         elif self.path == "/jpeg":
-            print "HANDLER jpeg headers",self.headers
+            print("HANDLER jpeg headers",self.headers)
             self.q = Queue()
             sourceJ.addlistener(self)
             while True:
                 try:
                     a = self.q.get(True,0.5) # make waitable
-                    if a == "": 
+                    if a == b"": 
                         break                        
-                except:
+                except Exception as e:
                     if sourceJ.done:
-                        print "source is done"
+                        print("source is done")
                         self.send_error(404)
                         break
                     else:
@@ -410,28 +414,28 @@ class RequestHandler(BaseHTTPRequestHandler):
                     try:
                         self._writeheaders(len(a))
                         self.wfile.write(a)
-                    except:
-                        print "HANDLER network exception",self.id
-                    print "HANDLER image deleivere drmoving"
+                    except Exception as e:
+                        print("HANDLER network exception",self.id)
+                    print("HANDLER /jpeg image deleivere drmoving")
                     break
             sourceJ.dellistener(self)
         elif self.path == "/mjpeg":
-            print "HANDLER mjpeg headers",self.headers
+            print("HANDLER mjpeg headers",self.headers)
             self._writeheaders()
 
             self.q = Queue()
-            print "HANDLER adding",self.id
+            print("HANDLER adding",self.id)
             sourceJ.addlistener(self)
             while True:
                 try:
                     a = self.q.get(True,0.5) # make waitable
-                    if a == "": 
+                    if a == b"": 
                         break
-                except:
-                    a = ""
+                except Exception as e:
+                    a = b""
                     if sourceJ.done:
                         #Note that the encapsulation boundary must occur at the beginning of a line, i.e., following a CRLF, and that that initial CRLF is considered to be part of the encapsulation boundary rather than part of the preceding part. 
-                        self.wfile.write("\r\n--BOUNDARY\r\n\r\n")
+                        self.wfile.write(b"\r\n--BOUNDARY\r\n\r\n")
                         break
                     else:
                         continue
@@ -439,13 +443,13 @@ class RequestHandler(BaseHTTPRequestHandler):
                 if len(a) != 0:
                     try:
                         #Note that the encapsulation boundary must occur at the beginning of a line, i.e., following a CRLF, and that that initial CRLF is considered to be part of the encapsulation boundary rather than part of the preceding part. 
-                        self.wfile.write("\r\n--BOUNDARY\r\n"+"Content-Type: image/jpeg\r\nContent-Length: %d\r\n\r\n" % len(a))
+                        self.wfile.write(b"\r\n--BOUNDARY\r\nContent-Type: image/jpeg\r\nContent-Length: %d\r\n\r\n" % len(a))
                         self.wfile.write(a)
-                    except:
-                        print "HANDLER network exception",self.id
+                    except Exception as e:
+                        print("HANDLER /mjpeg network exception",self.id,e)
                         break
             sourceJ.dellistener(self)
-            print "HANDLER removed",self.id
+            print("HANDLER removed",self.id)
         else:
             self.send_error(404)
 
@@ -462,7 +466,7 @@ if __name__ == '__main__':
 
     if args.test is not None:
         aa = sys.stdin if args.text == "-" else open(args.text,"rb")
-        prebuf = ""
+        prebuf = b""
         ss = StreamingAtom(False)
         ready = False
         t2t = dict()
@@ -475,10 +479,10 @@ if __name__ == '__main__':
                 break
             ss.data += stdoutdata
             for a,t,s in ss.ondata():
-                print "event",t,t2t[a],len(s) if type(s) is str else s
-                if not ready and a == StreamingAtom.BEGIN and t == "moof":
+                print("event",t,t2t[a],len(s) if type(s) is str else s)
+                if not ready and a == StreamingAtom.BEGIN and t == b"moof":
                     ready = True
-                    print "\t found head",len(prebuf)
+                    print("\t found head",len(prebuf))
                 elif a == StreamingAtom.BODY:
                     prebuf += s        
     else:
